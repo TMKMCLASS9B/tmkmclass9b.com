7cf97469247314e6c2648268f3da2c1635a67ab6
TMKMCLASS9B <tmkmclass9b@gmail.com>
2024-08-09 12:25:04 +0600

    Add files via upload

---
+++ allsub.js
@@ -0,0 +1,10 @@
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const hamburger = document.querySelector('.hamburger');
+        const navLinks = document.querySelector('.nav-links');
+
+        hamburger.addEventListener('click', function() {
+            navLinks.classList.toggle('active');
+        });
+    });
+</script>
